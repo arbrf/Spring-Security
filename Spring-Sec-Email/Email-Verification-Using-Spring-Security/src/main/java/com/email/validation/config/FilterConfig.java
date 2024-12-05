965afc65552ca7d525542ca94519cff3d9a6943c
Mohammad Shaik <133103759+arbrf@users.noreply.github.com>
2024-12-05 12:06:30 +0530

    Update FilterConfig.java

--- Spring-Sec-Email/Email-Verification-Using-Spring-Security/src/main/java/com/email/validation/config/FilterConfig.java
+++ Spring-Sec-Email/Email-Verification-Using-Spring-Security/src/main/java/com/email/validation/config/FilterConfig.java
@@ -4,6 +4,7 @@ import com.email.validation.controller.HttpsRedirectFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 public class FilterConfig {
@@ -16,9 +17,7 @@ public class FilterConfig {
         return registrationBean;
     }
 
-    import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
+
 
 
 
