b7168a59bef00cf45be653eafff27f3575871f1f
Mohammad Shaik <133103759+arbrf@users.noreply.github.com>
2024-12-05 12:05:57 +0530

    Update FilterConfig.java

--- Spring-Sec-Email/Email-Verification-Using-Spring-Security/src/main/java/com/email/validation/config/FilterConfig.java
+++ Spring-Sec-Email/Email-Verification-Using-Spring-Security/src/main/java/com/email/validation/config/FilterConfig.java
@@ -15,4 +15,17 @@ public class FilterConfig {
         registrationBean.addUrlPatterns("/accounts/*");
         return registrationBean;
     }
+
+    import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+
+
 }
